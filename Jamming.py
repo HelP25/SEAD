d1a74ac6734ca0c87d47bcb8ba701ceac8f997df
heLP25 <158417286+HelP25@users.noreply.github.com>
2024-02-12 09:34:15 +1100

    Update Jamming.py

--- Jamming.py
+++ Jamming.py
@@ -2,6 +2,10 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
+
+def from_dB(G):  # Definition of a function to convert from dB to whatever unit
+    return 10 ** (G / 10)
+
 # Creation of the different assets in the battlespace
 class sensor_iads:
     #Physical caracteristics
@@ -32,55 +36,41 @@ class sensor_iads:
         return np.linalg.norm(np.array((self.X, self.Y)) -
                                   np.array((aircraft.X, aircraft.Y)))
 
-    def detection(self, aircraft, jammer = None):#tells if the asset is within the detection range of a radar
-        if self.range(aircraft) <= self.get_detection_range(aircraft, jammer):
+    def detection(self, aircraft, jammers = None):#tells if the asset is within the detection range of a radar
+        if self.range(aircraft) <= self.get_detection_range(aircraft, jammers):
             return True
         return False
 
     #Function that calculates the range of the sensor, either or not it has been jammed
-    def get_detection_range(self, aircraft, jammer = None):
-        def from_dB(G):  # Definition of a function to convert from dB to whatever unit
-            return 10 ** (G / 10)
-
-        if jammer is None:#if there is no aircraft jamming, then the range is calculated as if the power of the jamming was
-                          #equal to 0
-            Pj = 0
-            Gj = SEAD_aircraft.Gj
-            Lj = SEAD_aircraft.Lj
-            Bj = SEAD_aircraft.Bj
-            # Calculation of the range
-            # Range when the jammer is the aircraft to keep out of the detection range of the radar
-            # detection_range = ((from_dB(self.Pt) * from_dB(self.G)**2 * aircraft.rcs * from_dB(self.l)**2)
-            #                  / ((4 * np.pi)**3 * from_dB(self.L) * from_dB(self.F) * 1.38e-23 * self.Br
-            #                     + (4 * np.pi * from_dB(self.L) * jammer.Pj * from_dB(Gj) * self.Br)
-            #                     / (from_dB(Lj) * Bj)))**0.5 / 1000
-
-            # Range when the jammer reduces the detection range to protect a friendly asset from being detected (depends on the
-            # distance between the jammer and the radar targeted)
-            detection_range = ((from_dB(self.Pt) * from_dB(self.G) ** 2 * aircraft.rcs * from_dB(self.l) ** 2)
-                / ((4 * np.pi) ** 3 * from_dB(self.L) * from_dB(self.F) * 1.38e-23 * self.Br
-                + (4 * np.pi * from_dB(self.L) * Pj * from_dB(Gj) * self.Br)
-                / (from_dB(Lj) * Bj))) ** 0.25 / 1000
-        else:
-            #Calculation of the range
-            # Range when the jammer is the aircraft to keep out of the detection range of the radar
-            # detection_range = ((from_dB(self.Pt) * from_dB(self.G)**2 * aircraft.rcs * from_dB(self.l)**2)
-            #                  / ((4 * np.pi)**3 * from_dB(self.L) * from_dB(self.F) * 1.38e-23 * self.Br
-            #                     + (4 * np.pi * from_dB(self.L) * jammer.Pj * from_dB(Gj) * self.Br)
-            #                     / (from_dB(Lj) * Bj)))**0.5 / 1000
-            # Range when the jammer reduces the detection range to protect a friendly asset from being detected (depends
-            # on the distance between the jammer and the radar targeted)
-            detection_range = ((from_dB(self.Pt) * from_dB(self.G) ** 2 * aircraft.rcs * from_dB(self.l) ** 2)
-                         / ((4 * np.pi) ** 3 * from_dB(self.L) * from_dB(self.F) * 1.38e-23 * self.Br
-                            + (4 * np.pi * from_dB(self.L) * jammer.Pj * from_dB(jammer.Gj) * self.Br)
-                            / ((self.range(jammer) * 1000) ** 2 * from_dB(jammer.Lj) * jammer.Bj))) ** 0.25 / 1000
+    def get_detection_range(self, aircraft, jammers = None):
+        #Calculation of the range
+        # Range when the jammer is the aircraft to keep out of the detection range of the radar
+        # detection_range = ((from_dB(self.Pt) * from_dB(self.G)**2 * aircraft.rcs * from_dB(self.l)**2)
+        #                  / ((4 * np.pi)**3 * from_dB(self.L) * from_dB(self.F) * 1.38e-23 * self.Br
+        #                     + (4 * np.pi * from_dB(self.L) * jammer.Pj * from_dB(Gj) * self.Br)
+        #                     / (from_dB(Lj) * Bj)))**0.5 / 1000
+        # Range when the jammer reduces the detection range to protect a friendly asset from being detected (depends
+        # on the distance between the jammer and the radar targeted)
+        detection_range = ((from_dB(self.Pt) * from_dB(self.G) ** 2 * aircraft.rcs * from_dB(self.l) ** 2)
+                     / ((4 * np.pi) ** 3 * from_dB(self.L) * from_dB(self.F) * 1.38e-23 * self.Br
+                        + self.jamming_power(jammers))) ** 0.25 / 1000
         print(detection_range)
-        #Outline of the range
+        # Outline of the range
         theta = np.linspace(0, 2 * np.pi, 100)
         x = self.X + detection_range * np.cos(theta)
         y = self.Y + detection_range * np.sin(theta)
         plt.plot(x, y, label='Range')
         return detection_range
+
+    def jamming_power(self, jammers):#Calculate the jamming power needed to determine the detection range
+        if jammers is None:#if there isn't any jammer that is jamming
+            return 0
+        power = 0
+        for jammer in jammers:
+            power += ((4 * np.pi * from_dB(self.L) * jammer.Pj * from_dB(jammer.Gj) * self.Br)
+                      /((self.range(jammer) * 1000) ** 2 * from_dB(jammer.Lj) * jammer.Bj))
+        return power
+
 
 
 class SEAD_aircraft:
@@ -94,7 +84,7 @@ class SEAD_aircraft:
         self.X = X
         self.Y = Y
         self.coordinates = [X, Y]
-        self.point, = plt.plot(self.X, self.Y, 'r^', markersize=10, label='SEAD aircraft')
+        self.point, = plt.plot(self.X, self.Y, 'r4', markersize=10, label='SEAD aircraft')
 
     def update(self,x = None,y = None):#to update the position of the aircraft on the map if it moved
         if x is None:
@@ -110,23 +100,24 @@ class SEAD_aircraft:
 
 #Test
 radar1 = sensor_iads(400, 300)
-EA18G = SEAD_aircraft(100, 300)
+EA18G = SEAD_aircraft(210, 300)
 print("EA18G is detected before jamming: ")
 print(radar1.detection(EA18G))
 plt.pause(2)
 F16 = SEAD_aircraft(362,342)
 print("EA18G is detected while jamming: ")
-print(radar1.detection(EA18G, EA18G))
+print(radar1.detection(EA18G, [EA18G]))
 print("F16 is detected while EA18G is jamming: ")
-print(radar1.detection(F16,EA18G))
+print(radar1.detection(F16,[EA18G]))
 plt.pause(4)
 EA18G.update(310)
 print('F16 is detected while EA18G is jamming: ')
-print(radar1.detection(F16,EA18G))
-
+print(radar1.detection(F16,[EA18G]))
+plt.pause(3)
+EA6B = SEAD_aircraft(330, 280)
+print('F16 is detected while EA18G is jamming: ')
+print(radar1.detection(F16,[EA18G, EA6B]))
 
 plt.legend()
 plt.show()
 
-plt.legend()
-plt.show()
